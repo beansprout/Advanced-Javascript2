af49448be57f84f057fed376245afd834e0fa600
beansprout <christinegierer@gmail.com>
2017-07-03 17:05:59 -0400

    updated

--- src/arrays.js
+++ src/arrays.js
@@ -2,19 +2,21 @@
 // These functions only need to work with arrays.
 
 /* eslint-disable no-unused-vars, max-len */
-
-const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
+const each = (elements, cb) => {
+  return elements.forEach(cb);
 };
 
 const map = (elements, cb) => {
+  return elements.map(cb);
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
 };
 
 const reduce = (elements, cb, memo) => {
+  return elements.reduce
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
